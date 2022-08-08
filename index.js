f8a0becff71ca6799d0a5ebad30a5c8ec5ed44b6
Titus Mumo <titusmumo@gmail.com>
2022-08-09 00:44:22 +0300

    Cmpleted assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+number = 5;
+function add() {
+    return (number +=5)
+}
+function subtract() {
+    return (number -=5)
+}
+function multiply() {
+    return (number *=5)
+}
+function divide() {
+    return (number /=5)
+}
+number = a = 20, b = 5;
+function add(a,b) {
+    return (a + b)
+} 
+function subtract(a,b) {
+    return (a -b)
+}
+function multiply(a,b) {
+    return (a*b)
+}
+function divide(a,b) {
+    return (a/b)
+}
+number = 344;
+function increment(n) {
+    return (n+1)
+}
+function decrement(n) {
+    return (n-1)
+}
+number = 9
+function makeInt(n) {
+    return (parseInt (n, 10))
+}
+function preserveDecimal(n) {
+    return (parseFloat(n))
+}
